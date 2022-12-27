4598251e4484b13277f70f420f98b0c98d3dbf3f
bibliobraggins <bibliobraggins@gmail.com>
2022-12-26 18:44:48 -0700

    initial plugin build

--- test/g7xx_native_test.exs
+++ test/g7xx_native_test.exs
@@ -3,7 +3,7 @@ defmodule G7XXTest do
   doctest G7XX.Native
 
   test "compress a single i16 to a ulaw companded u8" do
-    companded_u8 = 15 # 0x0f # 0b00001111 # <<15::8>>
+    companded_u8 = 15 # 0x0f # 0b00001111 # <<15::8>> # <<>>
 
     # need to triple check if this conversion is correct.
     # I get consistent values, that doesn't mean the quantization is correct until I prove it.
