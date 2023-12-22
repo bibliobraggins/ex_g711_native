12747f20f3d9b89e68d50b73e98d947f695e159b
Chloe Law <104333742+bibliobraggins@users.noreply.github.com>
2023-12-22 12:42:51 -0700

    Update g711u_encoder.ex

--- lib/g711u_encoder.ex
+++ lib/g711u_encoder.ex
@@ -1,49 +1,4 @@
 defmodule G711u.Encoder do
-  use Membrane.Filter
-
-  require Logger
-
-  def_input_pad(:input,
-    availability: :always,
-    demand_unit: :buffers,
-    mode: :pull,
-    caps: :any
-  )
-
-  def_output_pad(:output,
-    mode: :pull,
-    demand_unit: :buffers,
-    caps: :any
-  )
-
-  @impl true
-  def handle_init(_) do
-    state = %{}
-
-    {:ok, state}
-  end
-
-  @impl true
-  def handle_prepared_to_stopped(_ctx, state) do
-    {:ok, state}
-  end
-
-  @impl true
-  def handle_prepared_to_playing(_ctx, state) do
-    {:ok, state}
-  end
-
-  @impl true
-  def handle_demand(:output, size, :buffers, _context, state) do
-    {{:ok, demand: {:input, size}}, state}
-  end
-
-  @impl true
-  def handle_process(:input, %Membrane.Buffer{} = buffer, _context, state) do
-    Logger.debug(G7XX.Native.expand_ulaw_buffer(buffer.payload))
-
-    {{:ok, buffer: {:output, buffer}}, state}
-  end
 
   def handle_buffer(<<buffer::bitstring>>) do
     output = <<>>
